b3c7bc7dbb239890cbb2b3ccba58bf3b93b207e5
alexy <od-5@yandex.ru>
2017-02-15 19:57:09 +0300

    fix mail

--- apps/landing/ajax.py
+++ apps/landing/ajax.py
@@ -21,11 +21,11 @@ __author__ = 'alexy'
 @ajax_request
 @csrf_exempt
 def ticket(request):
-    name = request.POST.get('name')
-    phone = request.POST.get('phone')
-    mail = request.POST.get('mail')
-    theme = request.POST.get('theme')
-    city = request.POST.get('city')
+    name = request.POST.get('name') or ''
+    phone = request.POST.get('phone') or ''
+    mail = request.POST.get('mail') or ''
+    theme = request.POST.get('theme') or ''
+    city = request.POST.get('city') or ''
     headers = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'}
     values = {'name': name.encode('utf-8'), 'phone': phone.encode('utf-8'), 'mail': mail.encode('utf-8'), 'theme': theme.encode('utf-8'), 'city': city.encode('utf-8')}
     data = urllib.urlencode(values)
