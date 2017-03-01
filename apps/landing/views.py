7a69097ba2f4466a2af1db786fa27dfc62dd9777
alexy <od-5@yandex.ru>
2017-03-01 18:31:23 +0300

    fix index.html template

--- apps/landing/views.py
+++ apps/landing/views.py
@@ -1,10 +1,12 @@
 # coding=utf-8
 from annoying.decorators import ajax_request
+from annoying.functions import get_object_or_None
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import TemplateView, DetailView
 from django.views.generic.base import ContextMixin
+from django_geoip.models import IpRange
 
 from .models import City
 
@@ -31,10 +33,17 @@ class CityDetailView(DetailView, CityListMixin):
 
 
 def home_view(request):
-    # return HttpResponseRedirect(reverse('distributor:update', args=(distriutor.id,)))
-    location = request.location
+    ip = request.META.get('REMOTE_ATTR', '')
+    try:
+        geoip_record = IpRange.objects.by_ip(ip)
+        city_name = geoip_record.city.name
+        city = get_object_or_None(City, name__iexact=city_name)
+        if city:
+            return HttpResponseRedirect(reverse('landing:city', args=(city.slug,)))
+    except IpRange.DoesNotExist:
+        city_name = None
     context = {
         'city_list': City.objects.all(),
-        'location': location
+        'location': city_name
     }
     return render(request, 'index.html', context)
